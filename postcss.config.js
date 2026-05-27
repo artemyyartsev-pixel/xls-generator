dee367416e50402d1c3b9423ddaffa0c3ba6ed47
Yartsev <ayartsev@neoflex.ru>
2026-05-27 16:43:04 +0300

    Update dependencies: replace @types/multer with archiver, adjust imports, improve PostCSS config, and modify server routes

--- postcss.config.js
+++ postcss.config.js
@@ -1,4 +1,40 @@
-export default {
+
+/**
+ * PostCSS configuration.
+ *
+ * @type {import('postcss').ProcessOptions}
+ *
+ * @property {Object} plugins - The list of PostCSS plugins.
+ * @property {Object} plugins.tailwindcss - Tailwind CSS plugin configuration.
+ * @property {Object} plugins.autoprefixer - Autoprefixer plugin configuration.
+ */
+export default /** @type {import('postcss').ProcessOptions} */ ({
+  plugins: {
+    /** Tailwind CSS plugin */
+    tailwindcss: /** @type {object} */ ({}),
+
+    /** Autoprefixer plugin */
+    autoprefixer: /** @type {object} */ ({}),
+  },
+});
+/**
+ * PostCSS configuration.
+ *
+ * @type {import('postcss').ProcessOptions}
+ *
+ * @property {Object} plugins - The list of PostCSS plugins.
+ * @property {Object} plugins.tailwindcss - Tailwind CSS plugin configuration.
+ * @property {Object} plugins.autoprefixer - Autoprefixer plugin configuration.
+ */
+export default /** @type {import('postcss').ProcessOptions} */ ({
+  plugins: {
+    /** Tailwind CSS plugin */
+    tailwindcss: /** @type {object} */ ({}),
+
+    /** Autoprefixer plugin */
+    autoprefixer: /** @type {object} */ ({}),
+  },
+});
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
